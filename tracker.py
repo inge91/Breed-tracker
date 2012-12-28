2a6a3435a3530452b623d5d58041589a56e41ac5
Inge Becht <inge@crunchbang-vm.(none)>
2012-12-28 16:09:29 +0100

    retry duplicate finder

--- tracker.py
+++ tracker.py
@@ -67,12 +67,11 @@ def update_pug_list():
         if(find_breed(animal)):
             site = extract_site(animal)
             f = open('pugs.txt', 'a')
-            #wfile = f.readline()
-            #if re.search(site, wfile):
-            #    pass
-            #else: 
-            #    pass
-            f.write("<p><a href=" + site + ">" + site + "</a></p>")
+            wfile = f.read()
+            if re.search(site, wfile):
+                pass
+            else: 
+                f.write("<p><a href=" + site + ">" + site + "</a></p>")
 
 def main():
     port = int(os.environ.get('PORT', 5000))
