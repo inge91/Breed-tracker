bee66ba8587f678ac7b85b454b7f7082ba18b6f0
Inge Becht <inge@crunchbang-vm.(none)>
2012-12-28 16:01:21 +0100

    no more duplicate check, only write check

--- tracker.py
+++ tracker.py
@@ -67,12 +67,12 @@ def update_pug_list():
         if(find_breed(animal)):
             site = extract_site(animal)
             f = open('pugs.txt', 'a')
-            wfile = f.readline()
-            if re.search(site, wfile):
-                pass
-            else: 
-                pass
-            #    f.write("<p><a href=" + site + ">" + site + "</a></p>")
+            #wfile = f.readline()
+            #if re.search(site, wfile):
+            #    pass
+            #else: 
+            #    pass
+            f.write("<p><a href=" + site + ">" + site + "</a></p>")
 
 def main():
     port = int(os.environ.get('PORT', 5000))
